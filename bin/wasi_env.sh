812cd6ac48c63b653fbcab43c6e2a23eb2e06b78
pmp-p <mail.peny@free.fr>
2021-01-30 06:27:36 +0100

    fix target

--- bin/wasi_env.sh
+++ bin/wasi_env.sh
@@ -1,13 +1,17 @@
 
 export WASI_SDK_PATH=$(realpath $(dirname "$BASH_SOURCE")/../wasi-sdk)
-SYSROOT="--sysroot=$(realpath ${WASI_SDK_PATH}/share/wasi-sysroot) -include ${WD}/wasi-sdk/fix.h"
+SYSROOT="--sysroot=$(realpath ${WASI_SDK_PATH}/share/wasi-sysroot) --target=wasm32-unknow-wasi"
+FIX="-D__WASI__=1 -include ${WASI_SDK_PATH}/fix.h"
 
 
-export CC="$(command -v clang) -g0 -Os ${SYSROOT}"
-export CPP="clang ${SYSROOT} -E"
+export CC="$(command -v clang) -g0 -Os ${SYSROOT} $FIX"
+export CXX="$(command -v clang)++ -g0 -Os ${SYSROOT} $FIX"
+export CPP="clang ${SYSROOT} $FIX -E"
 
 echo " wasi sdk set as:
-$CC
+CC=$CC
+CXX=$CXX
+CPP=$CPP
 "
 
 
